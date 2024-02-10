dc9542029cc727b9a7d93c47b5533a61227dac47
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-02-10 20:46:46 +0600

    add new screen

--- flutter_push_notifications/lib/firebase_set/firebase_api.dart
+++ flutter_push_notifications/lib/firebase_set/firebase_api.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_push_notifications/main.dart';
+import 'package:flutter_push_notifications/page/notification_screen.dart';
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
   print("Title : ${message.notification?.title}");
@@ -9,10 +14,80 @@ Future<void> handleBackgroundMessage(RemoteMessage message) async {
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications',
+    importance: Importance.defaultImportance,
+  );
+
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) return;
+
+    navigatorKey.currentState?.pushNamed(
+      NotificationScreen.route,
+      arguments: message,
+    );
+  }
+
+  Future initLocalNotifications() async {
+    const android = AndroidInitializationSettings('@drawble/ic_launcher');
+    const settings = InitializationSettings(
+      android: android,
+    );
+
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (payload) {
+        final message = RemoteMessage.fromMap(jsonDecode(payload.toString()));
+        handleMessage(message);
+      },
+    );
+
+    final platform = _localNotifications.resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>();
+    await platform?.createNotificationChannel(_androidChannel);
+  }
+
+  Future initPushNotifications() async {
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+
+      if (notification == null) return;
+
+      _localNotifications.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@drawble/ic_launcher',
+          ),
+        ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
+  }
+
   Future<void> initNotificaions() async {
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
     print("Token : $fCMToken");
-    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    initPushNotifications();
+    initLocalNotifications();
   }
 }
---
+++ flutter_push_notifications/lib/page/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Push Notifications"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("HomePage"),
+      ),
+    );
+  }
+}
